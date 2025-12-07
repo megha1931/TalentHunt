998bb333f1537ed99c220f2c3b86ffe2dd5887a2
Megha Thakur <meghahere31@gmail.com>
2025-12-07 18:52:07 +0530

    deployment config added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,4 +5,5 @@ dotenv.config();
 export const ENV = {
   PORT: process.env.PORT,
   DB_url: process.env.DB_url,
+  NODE_ENV : process.env.NODE_ENV
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,26 @@
-import express from "express"
+import express from "express";
+import path from "path";
 import { ENV } from "./lib/env.js";
+
 const app = express();
+const __dirname = path.resolve();
 
-console.log(ENV.PORT);
+// ✅ Test API
+app.get("/hii", (req, res) => {
+  res.status(200).json({ msg: "success from api" });
+});
 
+// ✅ Production Mode Only
+if (ENV.NODE_ENV === "production") {
+     app.use(express.static(path.join(__dirname, "../frontend/dist"))); 
+    app.get("/{*any}", (req, res) => {
+         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")); 
+        }); 
+    }
 
-app.get("/",(req,res)=>{
-    res.status(200).json({msg:"succes from api"})
-})
+// ✅ Server Start
+app.listen(ENV.PORT, () => {
+  console.log(`✅ Server running on port ${ENV.PORT}`);
+  console.log(`✅ MODE = ${ENV.NODE_ENV}`);
+});
 
-app.listen(ENV.PORT)
