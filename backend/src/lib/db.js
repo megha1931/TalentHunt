31ddb7dcbc7288d22c4f0ba78d222499be12c3a2
Megha Thakur <meghahere31@gmail.com>
2025-12-08 15:47:49 +0530

    changes in db file

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import {ENV} from "./env.js"
 export const connectDB = async()=>{
     try{
+        if(!ENV_DB_URL){
+            throw new Error("DB_URL is not defined")
+        }
         const conn = await mongoose.connect(ENV.DB_URL)
          console.log("connected to db",conn.connection.host);
          
