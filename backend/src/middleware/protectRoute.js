8a1090769f4e75a4e8c8b0d3c46e3ef6c1af3464
Megha Thakur <meghahere31@gmail.com>
2025-12-09 13:47:45 +0530

    working modules

---
+++ backend/src/middleware/protectRoute.js
@@ -0,0 +1,26 @@
+import { clerkClient, requireAuth, getAuth } from "@clerk/express";
+
+import User from "../models/User.js";
+
+export const protectRoute = [
+  requireAuth({signInUrl:"/sign-in"}),
+  async (req, res, next) => {
+    try {
+      const clerkId = req.auth().userId;
+      if (!clerkId)
+        return res.status(401).json({ message: "unaothorized- invalid login" });
+
+      //find user in db by clerkId
+      const user = await User.findOne({ clerkId: clerkId });
+
+      if (!user) return res.status(404).json({ msg: "user not found" });
+      //attach user to request
+      req.useer = user;
+
+      next();
+    } catch (error) {
+      console.log("error in protectRoute middleware", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+];
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ import { connectDB } from "./lib/db.js";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
 import cors from "cors";
+import {clerkMiddleware} from '@clerk/express'
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 const __dirname = path.resolve();
@@ -13,6 +15,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 //credentials:true meaning ?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware())//adds auth field to request object req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
@@ -20,6 +23,12 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/hii", (req, res) => {
   res.status(200).json({ msg: "success from api" });
 });
+// when you pass an array of middleware to exress , it automatically flattens and executs them 
+// sequentially one by one
+app.get("/video-calls", protectRoute ,(req,res)=>{
+   res.status(200).json({ msg: "this is from video call end point" });
+
+})
 
 // ✅ Production Mode Only
 if (ENV.NODE_ENV === "production") {
