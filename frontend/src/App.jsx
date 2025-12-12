dca53f6a69301673145b54d8349ef02b82b6090c
Megha Thakur <meghahere31@gmail.com>
2025-12-12 16:59:54 +0530

    tailwind changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { useUser } from '@clerk/clerk-react'
 import { Navigate, Route, Routes } from 'react-router'
 import { Toaster } from 'react-hot-toast'
 import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
 import ProblemsPge from './pages/ProblemsPge'
 function App() {
   const {isSignedIn} = useUser()
